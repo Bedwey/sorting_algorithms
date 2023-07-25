b1a8295744191bcf84b3045ba9667967bde67872
Abanoubo1 <abanoubwageh88@gmail.com>
2023-07-25 12:14:57 -0700

    sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,34 +9,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *prev, *next;
+	listint_t *current, *prev, *next;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        next = current->next;
-        prev = current->prev;
+	while (current)
+	{
+		next = current->next;
+		prev = current->prev;
 
-        while (prev && prev->n > current->n)
-        {
-            prev->next = current->next;
-            if (current->next)
-                current->next->prev = prev;
-            current->next = prev;
-            current->prev = prev->prev;
-            prev->prev = current;
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
-            print_list(*list);
-            prev = current->prev;
-        }
-
-        current = next;
-    }
+		while (prev && prev->n > current->n)
+		{
+			prev->next = current->next;
+			if (current->next)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			prev->prev = current;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = next;
+	}
 }
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort_helper(int *array, int lo, int hi, size_t size);
 int lomuto_partition(int *array, int lo, int hi, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
