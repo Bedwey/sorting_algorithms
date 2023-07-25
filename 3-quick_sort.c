9a8f2a2f1431ab7390313a370758be19663d97d0
Bedwey <bedweyy@gmail.com>
2023-07-25 09:38:30 -0700

    fix betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,8 +10,8 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array && size > 1)
-        quick_sort_helper(array, 0, size - 1, size);
+	if (array && size > 1)
+		quick_sort_helper(array, 0, size - 1, size);
 }
 
 /**
@@ -27,14 +27,14 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_helper(int *array, int lo, int hi, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (lo < hi)
-    {
-        pivot = lomuto_partition(array, lo, hi, size);
-        quick_sort_helper(array, lo, pivot - 1, size);
-        quick_sort_helper(array, pivot + 1, hi, size);
-    }
+	if (lo < hi)
+	{
+		pivot = lomuto_partition(array, lo, hi, size);
+		quick_sort_helper(array, lo, pivot - 1, size);
+		quick_sort_helper(array, pivot + 1, hi, size);
+	}
 }
 
 /**
@@ -49,31 +49,31 @@ void quick_sort_helper(int *array, int lo, int hi, size_t size)
  */
 int lomuto_partition(int *array, int lo, int hi, size_t size)
 {
-    int pivot = array[hi];
-    int i = lo - 1, j, temp;
+	int pivot = array[hi];
+	int i = lo - 1, j, temp;
 
-    for (j = lo; j < hi; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 
-    if (array[i + 1] != array[hi])
-    {
-        temp = array[i + 1];
-        array[i + 1] = array[hi];
-        array[hi] = temp;
-        print_array(array, size);
-    }
+	if (array[i + 1] != array[hi])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = temp;
+		print_array(array, size);
+	}
 
-    return (i + 1);
+	return (i + 1);
 }
